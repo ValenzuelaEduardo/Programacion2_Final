61f1e709560c5e6d17e067eb9d69b3471c697bd1
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2024-11-26 13:08:55 -0300

    update de main e ingredientes crud

---
+++ ORM_clientes/CRUD/ingrediente_crud.py
@@ -0,0 +1,46 @@
+from sqlalchemy.exc import IntegrityError
+
+# Crear una sesión
+def get_session():
+    try:
+        db = "mysql+mysqlconnector://root:@localhost/Restaurante"()
+        yield db
+    finally:
+        db.close()
+
+#---------Create-------------------
+def crear_ingrediente(nombre, tipo, cantidad, unidad_medida, session):
+    try:
+        nuevo_ingrediente = Ingrediente(nombre=nombre, tipo=tipo, cantidad=cantidad, unidad_medida=unidad_medida)
+        session.add(nuevo_ingrediente)
+        session.commit()
+        print(f"Ingrediente '{nombre}' creado con éxito.")
+    except IntegrityError:
+        session.rollback()
+        print("Error: Ya existe un ingrediente con este nombre.")
+
+#---------Read-------------------
+def leer_ingredientes(session):
+    ingredientes = session.query(Ingrediente).all()
+    for ingrediente in ingredientes:
+        print(f"ID: {ingrediente.id_ingrediente}, Nombre: {ingrediente.nombre}, Tipo: {ingrediente.tipo}, Cantidad: {ingrediente.cantidad} {ingrediente.unidad_medida}")
+
+#---------Update-------------------
+def actualizar_ingrediente(id_ingrediente, nueva_cantidad, session):
+    ingrediente = session.query(Ingrediente).filter_by(id_ingrediente=id_ingrediente).first()
+    if ingrediente:
+        ingrediente.cantidad = nueva_cantidad
+        session.commit()
+        print(f"Ingrediente ID {id_ingrediente} actualizado.")
+    else:
+        print("Ingrediente no encontrado.")
+
+#---------Delete-------------------
+def eliminar_ingrediente(id_ingrediente, session):
+    ingrediente = session.query(Ingrediente).filter_by(id_ingrediente=id_ingrediente).first()
+    if ingrediente:
+        session.delete(ingrediente)
+        session.commit()
+        print(f"Ingrediente ID {id_ingrediente} eliminado.")
+    else:
+        print("Ingrediente no encontrado.")
